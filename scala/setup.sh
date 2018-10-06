56847c60358e0d20c0fece302b51f798c00b059f
Raphael Luckom <raphaelluckom@gmail.com>
2018-10-06 15:44:40 -0400

    update scala tools

---
+++ scala/setup.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+set -e
+
+docker build -t amm -f Dockerfile-amm .
+docker build -t sbt -f Dockerfile-sbt .
+
+mkdir -p ~/configs/scala-build/sbt-global-base
+mkdir -p ~/configs/scala-build/sbt-ivy-home
+mkdir -p ~/configs/scala-build/sbt-boot-directory
+
+mkdir ~/.bin
+
+ln -s $(pwd)/amm ~/.bin/amm
+ln -s $(pwd)/sbt ~/.bin/sbt
